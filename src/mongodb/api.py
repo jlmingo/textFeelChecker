5da5075e0109c1d1c8d2f51a986ce2e2e1f0853d
Jorge López-Mingo <jorgelopezmingo@gmail.com>
2019-12-01 16:12:01 +0100

    post

--- src/mongodb/api.py
+++ src/mongodb/api.py
@@ -1,12 +1,46 @@
+#!/usr/bin/env python3
+
 import requests
-from bottle import route, run
+from bottle import route, run, post, request
 from functions import connection
 from bson.json_util import dumps
 
 
 @route('/data')
 def data():
-    return dumps(collection.find())
+    return dumps(collection.find({}))
 
+@post('/user/create')
+def newUser():
+    name = str(request.forms.get("name"))
+    new_id = max(collection.distinct("idUser")) + 1
+    new_user = {
+        "idUser": new_id,
+        "userName": name
+    }
+    collection.insert_one(new_user)
+    print(f"{name} added to collection with id {new_id}") #this is not working :(
+
+@post('/chat/create')
+def chatCreate():
+    participants = list(request.forms.get("participants"))
+    new_id = max(collection.distinct("idChat")) + 1
+    new_chat = {
+        "idChat": new_id,
+        "Participants": participants
+    }
+    collection.insert_one(new_chat)
+    # return dumps(idChat)
+
+# @post("/chat/<chat_id>/addmessage")
+# def addMessage():
+#     participants = list(request.forms.get("participants"))
+#     new_id = max(collection.distinct("idChat")) + 1
+#     new_chat = {
+#         "idChat": new_id,
+#         "Participants": participants
+#     }
+#     collection.insert_one(new_chat)
+    
 database, collection = connection('datamad1019','chats')
 run(host='localhost', port=8080, debug=True)
--- src/mongodb/mongo.py
+++ src/mongodb/mongo.py
@@ -21,3 +21,9 @@ db, coll = connectCollection('datamad1019','chats')
 query = {'idChat':0}
 test_query = coll.find(query)
 print(list(test_query))
+
+
+# Add field
+df=pd.DataFrame(list(coll.find({})))
+df["Participants"] = df.idChat.apply(lambda x: list(df[df.idChat == x].idUser.unique()))
+coll.update({"_id": b["_id"]}, {"$set": {"geolocCountry": myGeolocCountry}})
